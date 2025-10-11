0f71634396ff80377efd1c5e2a443180a019e205
latha-svarna <latha.svarna@gmail.com>
2025-10-11 12:13:47 +0530

    Added Lab Programs.

--- src/ex_01_Java_Basics/Lab001_HelloWorld.java
+++ src/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -1,7 +1,40 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+package ex_01_Java_Basics;
+import java.lang.*;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Lab001_HelloWorld {
     public static void main(String[] args) {
-        System.out.printf("Hello World!");
+//        String str="Sswarnaa";
+//        char c[]=str.toCharArray();
+//        StringBuilder sd=new StringBuilder();
+//        Set<Character> unq=new HashSet<>();
+//        for(int i=0;i<c.length;i++)
+//        {
+//            if(!(unq.contains(c[i])))
+//            {
+//                unq.add(c[i]);
+//                sd.append(c[i]);
+//            }
+//        }
+//        System.out.println(sd);
+//        System.out.println(sd.reverse().toString());//
+//        System.out.println(str.replaceAll("\\s",""));
+        int a[]={7,2,3,4,5,8};
+        int first=0;
+        int second=0;
+        for(int i=0;i<a.length;i++)
+        {
+//            int j=i+1;
+            if(a[i]>first)
+            {
+                second=first;
+                first=a[i];
+
+            }
+            else if(a[i]>second)
+                second=a[i];
+            System.out.println(first+""+second);
+        }
     }
 }
